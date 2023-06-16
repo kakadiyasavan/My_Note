52d972b373531c326989c3efe659b67094d0d470
kakadiyasavan <kakadiyasavan3@gmail.com>
2023-06-16 15:33:48 +0530

    commite

--- app/src/main/java/com/example/my_note/Adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/my_note/Adapter/NoteAdapter.kt
@@ -1,28 +1,49 @@
 package com.example.my_note.Adapter
 
-import android.graphics.Color
+import android.app.Dialog
+import android.content.Context
+import android.os.Build
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.core.graphics.toColor
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.my_note.Database.RoomDB
 import com.example.my_note.Entity.NoteEntitty
-import com.example.my_note.databinding.NoteItemBinding
+import com.example.my_note.MainActivity
+import com.example.my_note.R
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class NoteAdapter(notes: List<NoteEntitty>) : Adapter<NoteAdapter.NoteHolder>() {
 
 
     var notes = notes
+    lateinit var context: Context
+    lateinit var db: RoomDB
 
-    class NoteHolder(itemView: NoteItemBinding) : ViewHolder(itemView.root) {
 
-        var binding = itemView
+    class NoteHolder(itemView: View) : ViewHolder(itemView) {
+
+
+        var texttitel=itemView.findViewById<TextView>(R.id.txtitle)
+        var textnote=itemView.findViewById<TextView>(R.id.txtnote)
+        var pin=itemView.findViewById<ImageView>(R.id.imgPin)
+        var cardnote=itemView.findViewById<CardView>(R.id.card)
+        var delete=itemView.findViewById<ImageView>(R.id.delete)
+        var edit=itemView.findViewById<ImageView>(R.id.edit)
+
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteHolder {
-        var binding = NoteItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return NoteHolder(binding)
+        context=parent.context
+       return  NoteHolder(LayoutInflater.from(parent.context).inflate(R.layout.note_item,parent,false))
     }
 
     override fun getItemCount(): Int {
@@ -32,17 +53,56 @@ class NoteAdapter(notes: List<NoteEntitty>) : Adapter<NoteAdapter.NoteHolder>() 
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: NoteHolder, position: Int) {
 
-        holder.binding.apply {
+
+        db = RoomDB.init(context)
+        holder.apply {
+
             notes.get(position).apply {
-                txtitle.text = title
-                txtnote.text = text
-                txtdate.text = date
+                texttitel.text = title
+                textnote.text = text
+
 
             }
-            txtitle.isSelected = true
-            card.setCardBackgroundColor(notes.get(position).color)
+
+            texttitel.isSelected = true
+            cardnote.setCardBackgroundColor(notes.get(position).color)
+
+            delete.setOnClickListener {
+                db.note().detelNote(notes[position])
+               update(notes)
+            }
+
+            itemView.setOnClickListener {
+                var dialog=Dialog(context)
+                dialog.setContentView(R.layout.activity_add)
+
+                var formater=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss a")
+                var curent=LocalDateTime.now().format(formater)
+
+                var edititle=dialog.findViewById<EditText>(R.id.editTitle)
+                var editnotes=dialog.findViewById<EditText>(R.id.editNote)
+
+                edititle.setText(notes[position].title)
+                editnotes.setText(notes[position].text)
+
+
+
+                dialog.show()
+
+                edit.setOnClickListener {
+                    var data=NoteEntitty(edititle.text.toString(),editnotes.text.toString(),curent,notes[position].color)
+                    data.id=notes[position].id
+
+                    db.note().updateNote(data)
+                    dialog.dismiss()
+                    update(notes)
+                }
+            }
+
+
         }
     }
 
